a08bd1ed0a5c1991219bc357c4c2296abc2b9107
dsusmit63 <dsusmit63@gmail.com>
2026-05-30 10:48:06 +0530

    added arithmetic_operators.py

---
+++ basics/arithmetic_operators.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 ** num2)   # Exponentiation
+print(num1 / num2)    # returns float
+print(num1 // num2)   # Floor Division, returns integer
+print(num1 % num2)

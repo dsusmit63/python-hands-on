c9a2587d4d3263f75046c9517a83a4d21e00756f
dsusmit63 <dsusmit63@gmail.com>
2026-05-30 12:16:09 +0530

    added calculate_percentage.py

---
+++ basics/calculate_percentage.py
@@ -0,0 +1,5 @@
+total_marks = 300
+print(f"Total marks: {total_marks} ")
+obtained_marks = int(input("Enter obtained marks: "))
+percentage_marks = obtained_marks * 100 / total_marks
+print(f"Percentage: {percentage_marks}")

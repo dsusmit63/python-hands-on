6238c14df25b70eff31ff06755f30c00a192d3b3
dsusmit63 <dsusmit63@gmail.com>
2026-05-30 12:10:06 +0530

    added find_average.py

---
+++ basics/find_average.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
+average = (num1 + num2 + num3)/3
+print(f"Average of {num1}, {num2}, and {num3} is: {int(average)}")

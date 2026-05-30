f49699d97eaa29709ac59f601e7b1e267411c885
dsusmit63 <dsusmit63@gmail.com>
2026-05-30 19:35:37 +0530

    added find_largest.py

---
+++ conditionals/find_largest.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+num3 = float(input("Enter num3: "))
+
+if num1 == num2 == num3:
+  print("All numbers are same")
+elif num1 >= num2 and num1 >= num3:
+  print("num1 is largest")
+elif num2 >= num1 and num2 >=num3:
+  print("num2 is largest")
+else:
+  print("num3 is largest")
+  

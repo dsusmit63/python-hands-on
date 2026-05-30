581cec8949692abed5b468b39b74fb0ef0b8e702
dsusmit63 <dsusmit63@gmail.com>
2026-05-30 10:44:17 +0530

    added basic_operations.py

---
+++ basics/basic_operations.py
@@ -0,0 +1,20 @@
+# String Concatination
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)          # John Doe
+print("Hello" + " World") # Hello World
+print("Hello" + "123")    # Hello123
+
+# print("Hello" + 123)    # Error
+# print("Hello" + 0.25)   # Error
+
+# print("Hello" + True)   # Error
+
+print(100 + 100.25)       # 200.25
+
+print(True + True)        # 2
+print(False + False)      # 0
+print(True + False)       # 1
+print(True + False + 2)   # 3
+print(True + False + 2.0) # 3.0

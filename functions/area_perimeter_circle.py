42b317b009c5ed5d47e81edcbac2ff7cb41dc0e5
dsusmit63 <dsusmit63@gmail.com>
2026-05-31 12:07:33 +0530

    added area_perimeter_circle.py

---
+++ functions/area_perimeter_circle.py
@@ -0,0 +1,7 @@
+radius = float(input("Enter radius of circle: "))
+def calculate_perimeter(radius):
+  print(f"The perimeter of the circle is: {2 * 3.14 * radius}")
+def calculate_area(radius):
+  print(f"The area of the circle is: {3.14 * radius * radius}")
+calculate_perimeter(radius)
+calculate_area(radius)

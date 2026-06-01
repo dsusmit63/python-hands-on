9723f4bc773444e599e30235f6158ff19fe35ac6
dsusmit63 <dsusmit63@gmail.com>
2026-06-01 11:43:26 +0530

    updated system_health.py

--- system-health/system_health.py
+++ system-health/system_health.py
@@ -1,7 +1,25 @@
 import psutil
+
 cpu_threshold = int(input("Enter CPU threshold: "))
+ram_threshold = int(input("Enter RAM threshold: "))
+
 cpu_usage = psutil.cpu_percent(interval=1)
-if cpu_usage > cpu_threshold:
-  print(f"Email Alert sent due to high cpu usage {cpu_usage}")
-else:
-  print(f"CPU is in safe mode. Current cpu usage {cpu_usage}")
+ram_usage = psutil.virtual_memory()
+total_gb = ram_usage.total / (1024 ** 3)
+used_gb = ram_usage.used / (1024 ** 3)
+available_gb = ram_usage.available / (1024 ** 3)
+
+def check_cpu():
+  if cpu_usage > cpu_threshold:
+    print(f"Email Alert sent due to high CPU usage {cpu_usage}")
+  else:
+    print(f"CPU is in safe mode. Current CPU usage {cpu_usage}")
+def check_ram():
+  if used_gb > ram_threshold:
+    print(f"Email Alert sent due to high RAM usage {used_gb}, available RAM {available_gb}")
+  else:
+    print(f"RAM is in safe mode. Current RAM usage {used_gb}, available RAM {available_gb}")
+
+for i in range(3):
+  check_cpu()
+  check_ram()
